c2bc4d002024b1ecf7d6b005f6d6d88dac9795b0
breduffy <breanne.duffy@gmail.com>
2015-07-09 18:00:46 -0400

    Code towards making Google Maps API markers work.

--- app/controllers/mapController.js
+++ app/controllers/mapController.js
@@ -7,16 +7,44 @@
       areaLng = -100.2471641,
       areaZoom = 3;
 
+    this.markers = [];
+    $scope.marker = {};
+    $scope.markers = [];
+
     var events = {
       places_changed: function(searchBox) {
-        console.log(searchBox.getPlaces());
+        var location = searchBox.getPlaces();
+        console.log(location[0].geometry.location.D)
+        console.log(location[0].geometry.location.k)
+        var marker = {
+          latitude: location[0].geometry.location.D,
+          longitude: location[0].geometry.location.k,
+          title: "our first marker",
+          id: 1
+        };
+        $scope.markers.push(marker);
       }
-    }
+    };
+
     $scope.searchbox = {
       template: 'searchbox.tpl.html',
       events: events
     };
 
+    // // map marker
+    // $scope.marker = {
+    //   id: 0,
+    //   coords: {
+    //     latitude: 7.0933,
+    //     longitude: 79.9989
+    //   },
+    //   options: {
+    //     //set this to true if you want the marker draggable
+    //     draggable: false,
+    //     title: 'The KVK Blog',
+    //     animation: 1 // 1: BOUNCE, 2: DROP
+    //   }
+    // };
 
     uiGmapGoogleMapApi.then(function(maps) {
       $scope.map = {
@@ -24,7 +52,8 @@
           latitude: areaLat,
           longitude: areaLng
         },
-        zoom: areaZoom
+        zoom: areaZoom,
+        draggable: true
       };
       $scope.options = {
         scrollwheel: false
