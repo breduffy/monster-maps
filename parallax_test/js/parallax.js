bfb01dff8cd922a8f9f1ece15c85a93fd6d57b54
breduffy <breanne.duffy@gmail.com>
2015-07-11 22:26:39 -0400

    Create parallax hero experiement.

--- parallax_test/js/parallax.js
+++ parallax_test/js/parallax.js
@@ -7,6 +7,21 @@ $(document).ready(function() {
   $('.dropdown-toggle').dropdown()
 
 
+  /* Scroll event handler */
+  $(window).bind('scroll', function(e) {
+    parallaxScroll();
+  });
+
+  /* Scroll the background layers */
+  function parallaxScroll() {
+    var scrolled = $(window).scrollTop();
+    $('#layer1').css('top', (0 - (scrolled * .25)) + 'px');
+    $('#layer2').css('top', (0 - (scrolled * .25)) + 'px');
+    $('#layer3').css('top', (0 - (scrolled * .5)) + 'px');
+    $('#layer4').css('top', (0 - (scrolled * .75)) + 'px');
+    $('#layer5').css('top', (0 - (scrolled * .75)) + 'px');
+    $('#layer6').css('top', (0 - (scrolled * .75)) + 'px');
+  }
 
 
 
